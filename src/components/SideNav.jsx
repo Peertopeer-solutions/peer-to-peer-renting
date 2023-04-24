d74eb27d3a7f92645163ab25189d1de6b9e80eda
personal <devesh.lakwal911@gmail.com>
2023-04-24 14:11:28 +0530

    size adjustment

--- src/components/SideNav.jsx
+++ src/components/SideNav.jsx
@@ -26,21 +26,21 @@ const SideNav = () => {
 
   return (
 
-    <div className="absolute top-12 bg-white rounded-xl flex flex-col p-6 shadow-2xl right-2">
-      <div className="flex flex-col ">
+    <div className=" absolute top-12 bg-white rounded-xl flex flex-col p-6 shadow-2xl right-2">
+      <div className="flex flex-col gap-2 text-[31px]">
         {user ? (
-          <div className='flex flex-col gap-2'> <Link to='/profile'>Profile</Link>
+          <div className=''> <Link to='/profile'>Profile</Link>
             <Link to=''>How it works </Link>
-            <Link className='text-blue-600' to="/requestedItems">Requested items</Link>
+            <Link className='text-blue-600 ' to="/requestedItems ">Requested items</Link>
             <Link className='text-blue-600' to="/rentalRequests">Requests</Link>
-            <button onClick={onLogout} className="border border-black">SignOut</button>
+            <button onClick={onLogout} className="w-2/4 bg-red-500 text-white rounded full">SignOut</button>
           </div>
 
         ) : (!user &&
-          <div className='flex flex-col gap-2'>
-            <Link to=''>How it works </Link>
+          <div className='flex flex-col gap-2 marker:justify-end'>
+            <Link className='' to=''>How it works </Link>
 
-            <Link to="/signin" className="bg-blue-600 text-center text-white p-1 px-2 rounded-full">Sign-In</Link>
+            <Link to="/signin" className="bg-blue-600 text-center text-[16px] text-white p-1 px-2 rounded-full">Sign-In</Link>
 
           </div>
         )}
