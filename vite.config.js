36f754cb3c903792d96043d151a7e36697ceb80e
devesh911 <62549616+devesh911@users.noreply.github.com>
2023-06-26 19:19:10 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -9,10 +9,5 @@ import svgr from 'vite-plugin-svgr'
 console.log(process.env.VITE_FIREBASE_API_KEY)
 export default defineConfig({
   plugins: [react(),svgr()],
-  define: {
-    'process.env.VITE_FIREBASE_API_KEY': JSON.stringify(process.env.VITE_FIREBASE_API_KEY),
-    'process.env.VITE_FIREBASE_AUTH_DOMAIN': JSON.stringify(process.env.VITE_FIREBASE_AUTH_DOMAIN),
-    'process.env.VITE_FIREBASE_PROJECT_ID': JSON.stringify(process.env.VITE_FIREBASE_PROJECT_ID),
-  },
 })
 
