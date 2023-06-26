5ae87987c703107a0b3f53831251f0193e3f35c4
personal <devesh.lakwal911@gmail.com>
2023-06-26 19:15:36 +0530

    bugfix

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr'
-import dotenv from 'dotenv';
+
 
 
 
 // https://vitejs.dev/config/
+console.log(process.env.VITE_FIREBASE_API_KEY)
 export default defineConfig({
   plugins: [react(),svgr()],
   define: {
