2edeb7d7be38d9bed85a2a7058170785f79ff14c
personal <devesh.lakwal911@gmail.com>
2023-06-26 17:44:55 +0530

    bugfix

--- vite.config.js
+++ vite.config.js
@@ -3,17 +3,15 @@ import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr'
 import dotenv from 'dotenv';
 
-const env = dotenv.config().parsed;
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(),svgr()],
   define: {
-    // Pass environment variables to your application
-    'process.env': Object.keys(env).reduce((acc, key) => {
-      acc[`import.meta.env.${key}`] = JSON.stringify(env[key]);
-      return acc;
-    }, {}),
+    'process.env.VITE_FIREBASE_API_KEY': JSON.stringify(process.env.VITE_FIREBASE_API_KEY),
+    'process.env.VITE_FIREBASE_AUTH_DOMAIN': JSON.stringify(process.env.VITE_FIREBASE_AUTH_DOMAIN),
+    'process.env.VITE_FIREBASE_PROJECT_ID': JSON.stringify(process.env.VITE_FIREBASE_PROJECT_ID),
   },
-
 })
+
