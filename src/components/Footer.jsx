c589cc8b68c5b7d10e50a43e6870097240616727
personal <devesh.lakwal911@gmail.com>
2023-06-30 00:46:50 +0530

    layout changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 import { Link } from 'react-router-dom'
 import { ReactComponent as Drone } from '../assets/illustration/Drone.svg'
-import { AtSymbolIcon, PhoneIcon } from '@heroicons/react/24/solid'
+
 
 
 const Footer = () => {
@@ -66,12 +66,17 @@ const Footer = () => {
           </div>
           <div className='flex flex-col gap-2'>
             <div className='flex gap-2'>
-            <AtSymbolIcon className='h-6' />
+            <svg className="h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+  <path strokeLinecap="round" d="M16.5 12a4.5 4.5 0 11-9 0 4.5 4.5 0 019 0zm0 0c0 1.657 1.007 3 2.25 3S21 13.657 21 12a9 9 0 10-2.636 6.364M16.5 12V8.25" />
+</svg>
           <p >support@rentivity.in</p>
             </div>
         
             <div className='flex gap-2'>
-            <PhoneIcon className='h-6' />
+            <svg className="h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 002.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 01-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 00-1.091-.852H4.5A2.25 2.25 0 002.25 4.5v2.25z" />
+</svg>
+
           <p >+911234567890</p>
             </div>
         
