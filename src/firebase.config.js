8e2adb56fd009ce854dd59f914221866c2884d94
personal <devesh.lakwal911@gmail.com>
2023-08-19 15:00:13 +0530

    Env update

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -7,12 +7,11 @@ import {getFunctions} from 'firebase/functions'
 
 export const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN ,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID  ,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER  ,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID
-
+  authDomain: "awesome-renting.firebaseapp.com",
+  projectId: "awesome-renting",
+  storageBucket: "awesome-renting.appspot.com",
+  messagingSenderId: "702713068942",
+  appId: "1:702713068942:web:6620f924cf541a261238df"
 };
 
 // Initialize Firebase
@@ -22,3 +21,4 @@ export const db = getFirestore(app)
 export const messaging = getMessaging(app);
 export const functions = getFunctions(app);
 
+// "AIzaSyByBWbpNlNbLlg5BrbzJc-DQ5pd_S-0IB8"
