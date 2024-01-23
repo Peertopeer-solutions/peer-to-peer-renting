0d24f2674d59b4e6d8e9646ac084ee117e1e4d42
Vineet <79019403+snghvineet@users.noreply.github.com>
2024-01-24 03:01:51 +0530

    Added text and password input component for forms

--- src/components/Form/Input.jsx
+++ src/components/Form/Input.jsx
@@ -1,7 +1,8 @@
 import { useState } from 'react';
 import { IoIosEye, IoIosEyeOff } from 'react-icons/io';
 
-export function TextInput({ id, label, options, type }) {
+export function TextInput({ id, label, options, error }) {
+	const borderColor = error ? 'focus:border-red-300' : 'focus:border-blue-300';
 	return (
 		<div className='flex flex-col gap-1'>
 			<label htmlFor={id} className='text-gray-500 text-sm font-bold'>
@@ -11,13 +12,17 @@ export function TextInput({ id, label, options, type }) {
 				id={id}
 				type='text'
 				{...options}
-				className='py-2 px-4 border-2 rounded-lg outline-none focus:border-blue-300'
+				className={'py-2 px-4 border-2 rounded-lg outline-none ' + borderColor}
 			/>
+			{error && <div className='text-red-400'>{error.message}</div>}
 		</div>
 	);
 }
 
-export function PasswordInput({ id, label, options }) {
+export function PasswordInput({ id, label, options, error }) {
+	const borderColor = error
+		? 'focus-within:border-red-300'
+		: 'focus-within:border-blue-300';
 	const [show, setShow] = useState(false);
 	function toggleShowPassword() {
 		setShow((show) => !show);
@@ -27,7 +32,11 @@ export function PasswordInput({ id, label, options }) {
 			<label htmlFor={id} className='text-gray-500 text-sm font-bold'>
 				{label}
 			</label>
-			<div className='flex border-2 pr-2 overflow-hidden rounded-lg focus-within:border-blue-300'>
+			<div
+				className={
+					'flex border-2 pr-2 overflow-hidden rounded-lg ' + borderColor
+				}
+			>
 				<input
 					id={id}
 					type={show ? 'text' : 'password'}
@@ -42,6 +51,7 @@ export function PasswordInput({ id, label, options }) {
 					{show ? <IoIosEyeOff /> : <IoIosEye />}
 				</button>
 			</div>
+			{error && <div className='text-red-400'>{error.message}</div>}
 		</div>
 	);
 }
