02cd83e588c71311c1b10def499c986ac9861b74
Vineet <79019403+snghvineet@users.noreply.github.com>
2024-03-13 01:51:26 +0530

    Added reducer for ui

--- src/components/Layout/RootLayout.jsx
+++ src/components/Layout/RootLayout.jsx
@@ -5,15 +5,27 @@ import { FirebaseAuthProvider } from '../../FirebaseAuthContext';
 const Footer = lazy(() => import('../Footer'));
 import Navbar from '../Navbar';
 import PageWrapper from './PageWrapper';
+import Banner from '../Design/Banner';
+
+import { useSelector } from 'react-redux';
+
 const RootLayout = () => {
 	const { pathname } = useLocation();
 
 	useEffect(() => {
 		window.scrollTo(0, 0);
 	}, [pathname]);
+
+	const { isBannerVisible } = useSelector((state) => state.ui);
 	return (
 		<>
 			<FirebaseAuthProvider>
+				{isBannerVisible && (
+					<Banner
+						text='You might not be able to use few features as you have not verified your email address.'
+						linkText='Verify now'
+					/>
+				)}
 				<header>
 					<Navbar />
 				</header>
