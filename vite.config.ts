8f74977220a79201ffbc4da0295b864a9aa96376
Vineet <79019403+snghvineet@users.noreply.github.com>
2024-03-13 00:24:39 +0530

    Manual chunking

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,23 @@ export default defineConfig({
 		setupFiles: ['./test/setup-test.ts'],
 		environment: 'jsdom',
 	},
+	build: {
+		// Fix: https://stackoverflow.com/questions/69260715/skipping-larger-chunks-while-running-npm-run-build
+		rollupOptions: {
+			output: {
+				manualChunks(id) {
+					if (id.includes('node_modules')) {
+						return id
+							.toString()
+							.split('node_modules/')[1]
+							.split('/')[0]
+							.toString();
+					}
+				},
+			},
+		},
+	},
+
 	// test: {
 	// 	globals: true,
 	// 	environment: 'jsdom',
