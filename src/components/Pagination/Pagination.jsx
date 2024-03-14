94578e6f529e530fb541432dfe9f5248215f8199
Vineet <79019403+snghvineet@users.noreply.github.com>
2024-03-14 23:16:42 +0530

    Moved pagination logic to redux and updated pagination look

--- src/components/Pagination/Pagination.jsx
+++ src/components/Pagination/Pagination.jsx
@@ -1,23 +1,41 @@
 import React from 'react';
 import PaginationItem from './PaginationItem';
+import { useDispatch } from 'react-redux';
+import { productsAction } from '../../store/products-slice';
+import { FaArrowLeft } from 'react-icons/fa';
+import { IoArrowBack, IoArrowForward } from 'react-icons/io5';
 
 const Pagination = ({ currentPage, totalItems, onPageChange }) => {
+	const dispatch = useDispatch();
+	function updateCurrentPage(pageNumber) {
+		dispatch(productsAction.setPage({ page: pageNumber }));
+	}
 	const totalPages = Math.ceil(totalItems / 8);
 
 	const pages = [];
+	// pages.push();
 	for (let i = 1; i <= totalPages; i++) {
 		pages.push(
 			<PaginationItem
 				key={`page-number-${i}`}
-				pageNumber={i}
 				isCurrentPage={currentPage === i}
-				onClick={onPageChange.bind(null, i)}
-			/>
+				onClick={updateCurrentPage.bind(null, i)}
+			>
+				{i}
+			</PaginationItem>
 		);
 	}
 	return (
 		<div className='m-3 mx-auto max-w-max '>
-			<ul className='space-x-2'>{pages}</ul>
+			<ul className='border flex border-gray-300 rounded overflow-hidden'>
+				<PaginationItem>
+					<IoArrowBack className='text-lg' />
+				</PaginationItem>
+				{pages}
+				<PaginationItem>
+					<IoArrowForward className='text-lg' />
+				</PaginationItem>
+			</ul>
 		</div>
 	);
 };
