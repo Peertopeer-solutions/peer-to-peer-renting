21caf1ef9de6c64a3842bbc5f904beedc7a8b6ce
Vineet <79019403+snghvineet@users.noreply.github.com>
2024-03-15 15:17:43 +0530

    Updated tests to handle new components

--- test/components/PaginationItem.test.tsx
+++ test/components/PaginationItem.test.tsx
@@ -5,15 +5,10 @@ import { vi, expect, describe, test, afterEach } from 'vitest';
 afterEach(cleanup);
 
 describe('Pagination Item Component', () => {
-	test('test if pagination item is being rendered', async () => {
-		render(<PaginationItem pageNumber={2} />);
-		expect(screen.getByText('2')).toBeInTheDocument();
-	});
-
 	test('test that on click works', async () => {
 		const onPageChangeMock = vi.fn();
 
-		render(<PaginationItem onClick={onPageChangeMock} />);
+		render(<PaginationItem onClick={onPageChangeMock}></PaginationItem>);
 		const button = screen.getByRole('button');
 		fireEvent.click(button);
 		screen.debug();
